1658f74aa56995d2f577a501a8a408e08463ced9
raulino1999 <cobraulino@gmail.com>
2026-02-19 16:00:09 +0000

    Create .c

---
+++ .c
@@ -0,0 +1,25 @@
+#include "ft_printf.h"
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		i;
+	int		count;
+
+	i = 0;
+	count = 0;
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1])
+		{
+			i++;
+			count += ft_format(format[i], args);
+		}
+		else
+			count += ft_putchar(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
